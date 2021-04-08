a8e4ada64b27f67c63bc5e0a424289aab8362e4a
Melina Garcia Carniel <melina.carniel@luizalabs.com>
2021-04-08 01:21:16 +0000

    Update sonar.sh

--- scripts/sonar.sh
+++ scripts/sonar.sh
@@ -12,7 +12,4 @@ PACKAGE_VERSION=$(cat package.json \
   | sed 's/[",]//g' \
   | tr -d '[[:space:]]')
 
-node_modules/.bin/sonar-scanner -Dsonar.host.url=$SONAR_HOST_URL -Dsonar.projectVersion=$PACKAGE_VERSION 
--Dsonar.login=$SONAR_SECRET_TOKEN -Dsonar.projectKey=$APP_NAME -Dsonar.projectName=$APP_NAME -Dsonar.gitlab.commit_sha=$CI_COMMIT_SHA 
--Dsonar.gitlab.ref_name=$CI_COMMIT_REF_NAME -Dsonar.gitlab.project_id=$CI_PROJECT_ID -Dsonar.gitlab.ci_merge_request_iid=$CI_MERGE_REQUEST_IID 
--Dsonar.qualitygate.wait=true
+node_modules/.bin/sonar-scanner -Dsonar.host.url=$SONAR_HOST_URL -Dsonar.projectVersion=$PACKAGE_VERSION -Dsonar.login=$SONAR_SECRET_TOKEN -Dsonar.projectKey=$APP_NAME -Dsonar.projectName=$APP_NAME -Dsonar.gitlab.commit_sha=$CI_COMMIT_SHA -Dsonar.gitlab.ref_name=$CI_COMMIT_REF_NAME -Dsonar.gitlab.project_id=$CI_PROJECT_ID -Dsonar.gitlab.ci_merge_request_iid=$CI_MERGE_REQUEST_IID -Dsonar.qualitygate.wait=true
