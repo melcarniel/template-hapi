8365ff56500cb9e1a7ef75899e6cd2a42374b2df
Antonio <antonio.junior@luizalabs.com>
2021-04-13 16:59:16 -0300

    chore: fortify env

--- scripts/fortify.sh
+++ scripts/fortify.sh
@@ -7,13 +7,6 @@ APP_NAME=$(cat package.json \
   | sed 's/[",]//g' \
   | tr -d '[[:space:]]')
 
-PACKAGE_VERSION=$(cat package.json \
-  | grep version \
-  | head -1 \
-  | awk -F: '{ print $2 }' \
-  | sed 's/[",]//g' \
-  | tr -d '[[:space:]]')
-
 sed -i "s/TPL_FORTIFY_CLIENT_TOKEN/$FORTIFY_CLIENT_TOKEN/g" ./fortify.properties
 cp ./fortify.properties /etc/fortify/fortify.properties
 java -jar /etc/fortify/fortify-api.jar create -name $APP_NAME -version master
